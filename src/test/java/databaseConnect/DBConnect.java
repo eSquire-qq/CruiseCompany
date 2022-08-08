a408986b7a2ec2b54b1fd1749aa1013e0f188033
Artem Teslia <eSquire.github.repos@gmail.com>
2022-08-08 11:27:08 +0300

    Update DBConnect

--- src/test/java/databaseConnect/DBConnect.java
+++ src/test/java/databaseConnect/DBConnect.java
@@ -12,16 +12,33 @@ public class DBConnect {
 
     private static final Logger logger = LoggerFactory.getLogger(DBConnect.class);
 
-    private final static BasicDataSource dataSource = new BasicDataSource();
+    private final static BasicDataSource dataSourceTest = new BasicDataSource();
 
     private static final DBConnect dbcp = new DBConnect();
 
+
+    private String url ;
+    private String username ;
+    private String password  ;
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public Connection getConnection() throws SQLException {
         try {
-            return dataSource.getConnection();
+            return dataSourceTest.getConnection();
         } catch (SQLException e) {
             logger.info("Connection successfully", e);
-            return dataSource.getConnection();
+            return dataSourceTest.getConnection();
         }
 
     }
@@ -31,12 +48,12 @@ public class DBConnect {
     }
 
     private DBConnect(){
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/ANDRIY");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("e12323");
-        dataSource.setMinIdle(5);
-        dataSource.setMaxIdle(10);
-        dataSource.setMaxOpenPreparedStatements(100);
+        dataSourceTest.setUrl(url);
+        dataSourceTest.setUsername(username);
+        dataSourceTest.setPassword(password);
+        dataSourceTest.setMinIdle(5);
+        dataSourceTest.setMaxIdle(10);
+        dataSourceTest.setMaxOpenPreparedStatements(100);
     }
 
 }
