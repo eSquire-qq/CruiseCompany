87ba1f51cf53f115742f47cd0b6e376131bebd68
Artem Teslia <eSquire.github.repos@gmail.com>
2022-08-08 11:25:15 +0300

    Add new SQL request for admin

--- src/main/java/com/example/cruisecompany/database/SQLRequests.java
+++ src/main/java/com/example/cruisecompany/database/SQLRequests.java
@@ -2,9 +2,9 @@ package com.example.cruisecompany.database;
 
 public class SQLRequests {
 
-    public static final String CREAT_USER = "INSERT INTO \"user\" (name, surname, phone_number, role_id, password) VALUES (?,?,?,?,?)";
+    public static final String CREAT_USER = "INSERT INTO \"user\" (name, surname, phone_number, role_id, password, email) VALUES (?,?,?,?,?,?)";
     public static final String READ_USER = "SELECT name, surname, phone_number, role_id FROM \"user\" WHERE id = ?";
-    public static final String UPDATE_USER = "UPDATE \"user\" SET name = ?, surname = ?, phone_number = ?, role_id = ?, password = ? WHERE passenger_id = ?";
+    public static final String UPDATE_USER = "UPDATE \"user\" SET name = ?, surname = ?, phone_number = ?, role_id = ?, password = ?, email = ? WHERE passenger_id = ?";
     public static final String DELETE_USER = "DELETE FROM \"user\" WHERE id = ?";
 
     public static final String CREAT_CRUISE = "INSERT INTO \"cruise\" (duration, price, cruise_start_date, cruise_end_date, cruise_liner_name,passenger_capacity,status)\" +\n" +
@@ -19,6 +19,10 @@ public class SQLRequests {
     public static final String DELETE_USER_CRUISE = "DELETE FROM \"user_cruise\" WHERE ticket_id = ?";
 
 
+    public static final String CREATE_ADMIN = "INSERT INTO \"user\" (name, surname, phone_number, role_id, password, email) VALUES (?,?,?,?,?,?)";
+    public static final String READ_ADMIN = "SELECT name, surname, phone_number, role_id FROM \"user\" WHERE id = ?";
+    public static final String UPDATE_ADMIN = "UPDATE \"user\" SET name = ?, surname = ?, phone_number = ?, role_id = ?, password = ?, email = ? WHERE passenger_id = ?";
+    public static final String DELETE_ADMIN = "DELETE FROM \"user\" WHERE id = ?";
 
     public static final String DATA_VERIFICATION = "SELECT phone_number, password FROM \"user\" WHERE phone_number = ? AND password = ? ";
 
