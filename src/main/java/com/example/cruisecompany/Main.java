111874c2aff884f49e81c1658a346fee9dcabdcf
Artem Teslia <eSquire.github.repos@gmail.com>
2022-08-16 15:03:51 +0300

    Small changes

--- src/main/java/com/example/cruisecompany/Main.java
+++ src/main/java/com/example/cruisecompany/Main.java
@@ -18,12 +18,7 @@ import static com.example.cruisecompany.entity.CruiseStatus.*;
 public class Main {
     public static void main(final String[] args) throws ClassNotFoundException {
 
-        //AdminDAO adminDAO = AdminDAO.getAdminInstance();
-        CruiseDAO cruiseDAO = CruiseDAO.getCruiseInstance();
 
-        //cruiseDAO.read(new Cruise(10L, 100.000, java.time.LocalDate.now(), java.time.LocalDate.now(),"Viking", 800,REGISTERED, 100));
-
-        //adminDAO.create(new User(20L,"name1", "surname1", "phoneNumber1", ADMINISTRATOR, "password1", "email1"));
 
     }
 }
