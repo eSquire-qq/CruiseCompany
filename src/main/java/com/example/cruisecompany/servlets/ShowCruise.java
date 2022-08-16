0cf844582f1444c1e2c6930d2eba4b6c854f558f
Artem Teslia <eSquire.github.repos@gmail.com>
2022-08-16 15:02:48 +0300

    Small changes

--- src/main/java/com/example/cruisecompany/servlets/ShowCruise.java
+++ src/main/java/com/example/cruisecompany/servlets/ShowCruise.java
@@ -1,7 +1,6 @@
 package com.example.cruisecompany.servlets;
 
 import com.example.cruisecompany.dao.CruiseDAO;
-import com.example.cruisecompany.database.DBCPDataSource;
 import com.example.cruisecompany.entity.Cruise;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
@@ -10,25 +9,20 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "ShowCruise", value = "/ShowCruise")
+@WebServlet(name = "ShowCruise", value = "/ShowAllCruise")
 public class ShowCruise extends HttpServlet {
 
     CruiseDAO cruiseDAO = CruiseDAO.getCruiseInstance();
-    Cruise cruise = new Cruise();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/ShowAllCruise.jsp");
+        requestDispatcher.forward(request,response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         List<Cruise> cruises = cruiseDAO.readAll();
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/ShowCruise.jsp");
         request.setAttribute("cruises",cruises);
-        requestDispatcher.forward(request,response);
-
     }
 }
