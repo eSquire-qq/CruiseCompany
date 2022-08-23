d4ff036d6ae09d341481097bc85c3755bec4ff17
Artem Teslia <eSquire.github.repos@gmail.com>
2022-08-23 10:56:06 +0300

    Add cruise information servlet

---
+++ src/main/java/com/example/cruisecompany/servlets/CruiseInformation.java
@@ -0,0 +1,29 @@
+package com.example.cruisecompany.servlets;
+
+import com.example.cruisecompany.dao.UserCruiseDAO;
+import com.example.cruisecompany.entity.Cruise;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "CruiseInformation", value = "/Information")
+public class CruiseInformation extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/CruiseInformation.jsp");
+        requestDispatcher.forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Long id = Long.valueOf(request.getParameter("id"));
+        UserCruiseDAO userCruiseDao = UserCruiseDAO.getUserCruiseInstance();
+        userCruiseDao.addToOrder(id);
+
+        Cruise cruise = userCruiseDao.addToOrder(id);
+        request.getSession().setAttribute("cruise",cruise);
+    }
+}
