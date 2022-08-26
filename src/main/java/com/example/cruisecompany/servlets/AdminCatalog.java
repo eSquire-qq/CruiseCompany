00a33e099d2d234b545fef872ffa4bb05957ce6a
Artem Teslia <eSquire.github.repos@gmail.com>
2022-08-26 13:55:01 +0300

    Admin can change users ticket status

---
+++ src/main/java/com/example/cruisecompany/servlets/AdminCatalog.java
@@ -0,0 +1,26 @@
+package com.example.cruisecompany.servlets;
+
+import com.example.cruisecompany.dao.CruiseDAO;
+import com.example.cruisecompany.entity.Cruise;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "AdminCatalog", value = "/AdminCatalog")
+public class AdminCatalog extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/main/AdminCatalog.jsp");
+        requestDispatcher.forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        CruiseDAO cruiseDAO = CruiseDAO.getCruiseInstance();
+        List<Cruise> cruises = cruiseDAO.readAllForAdmin();
+        request.getSession().setAttribute("cruise", cruises);
+    }
+}
