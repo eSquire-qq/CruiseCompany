00931cef64fb49fec5ece4b72d6f74094e348936
Artem Teslia <eSquire.github.repos@gmail.com>
2022-09-08 11:08:20 +0300

    Small changes

--- src/main/java/com/example/cruisecompany/service/ValidationForm.java
+++ src/main/java/com/example/cruisecompany/service/ValidationForm.java
@@ -10,25 +10,16 @@ public class ValidationForm {
 
         boolean valid = true;
 
-        if(user == null || user.getEmail() == null || user.getPhoneNumber() == null || user.getPassword() == null
-                || user.getName() == null || user.getSurname() == null){
-            valid = false;
-        }
-
-        if(!Pattern.matches("(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))", user.getEmail())){
+        if (!Pattern.matches("(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3})|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))", user.getEmail()))
             valid = false;
 
-        }
-
-        if(!Pattern.matches("[0-9a-zA-Z!@#$%^&*]{6,}",user.getPassword())) {
+        if (!Pattern.matches("(\\s*)?(\\+)?([- _():=+]?\\d[- _():=+]?){10,14}(\\s*)?", user.getPhoneNumber()))
             valid = false;
-        }
 
-        if(!Pattern.matches("(\\s*)?(\\+)?([- _():=+]?\\d[- _():=+]?){10,14}(\\s*)?",user.getPhoneNumber())){
+        if (!Pattern.matches("[0-9a-zA-Z!@#$%^&*]{6,}", user.getPassword()))
             valid = false;
-        }
 
         return valid;
-
     }
+
 }
