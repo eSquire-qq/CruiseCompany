c4ada0d5453e9018e208751a09d9ab7f61fe73e2
Artem Teslia <eSquire.github.repos@gmail.com>
2022-09-10 12:30:44 +0300

    Small changes

--- src/main/java/com/example/cruisecompany/servlets/user/Payments.java
+++ src/main/java/com/example/cruisecompany/servlets/user/Payments.java
@@ -20,12 +20,15 @@ public class Payments extends HttpServlet {
 
     private static CruiseDAO cruiseDAO = CruiseDAO.getCruiseInstance();
 
+    private static UserCruiseDAO userCruiseDAO = UserCruiseDAO.getUserCruiseInstance();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Long id = Long.valueOf(request.getParameter("id"));
         Integer ticketId = Integer.valueOf(request.getParameter("ticketId"));
 
         Cruise cruise = cruiseDAO.readOneCruise(id);
+        UserCruiseDAO.getUserCruiseInstance().activeUserOrder(ticketId);
 
         request.setAttribute("cruisePrice",cruise.getPrice());
         request.setAttribute("ticketId",ticketId);
@@ -46,8 +49,10 @@ public class Payments extends HttpServlet {
         String fileName = file.getSubmittedFileName();
 
         for (Part part : request.getParts()) {
-            part.write("D:\\upload\\" + fileName);
+            part.write("documents\\" + fileName);
         }
+
+        userCruiseDAO.updateDocument(fileName,ticketId);
 
         UserCruiseDAO.getUserCruiseInstance().changeBalance(cruisePrice,user.getId());
         user.setBalance(user.getBalance()-cruisePrice);
